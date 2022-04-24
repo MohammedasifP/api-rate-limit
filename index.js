ff9cfd1e1f83109d4a9b8816adbf52ac6328be1d
Mohammedasif Pinitod <87132170+MohammedasifP@users.noreply.github.com>
2022-04-24 19:40:25 +0530

    done

---
+++ index.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const app=express();
+const rateLimit=require('express-rate-limit');
+const limiter=rateLimit({
+    max:10,
+    windowMs:1000*60
+})
+
+app.get("/",limiter,async(req,res)=>{
+    try {
+        res.send("hello")
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+app.listen(4000,()=>{
+    console.log("listening on port 4000")
+})
